3130e99ad08215bbceacc244593329e7eee13530
Harshul <harshulkala14@gmail.com>
2026-03-14 18:18:39 +0530

    added frontend and login system

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const mysql = require("mysql2");
 
-app.get("/",(req,res)=>{
-res.send("Synapse server running")
-})
+const app = express();
 
-app.listen(3000,()=>{
-console.log("Server running on port 3000")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "yourpassword",
+  database: "synapse",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/signup", (req, res) => {
+  const { username, email, password } = req.body;
+
+  db.query(
+    "INSERT INTO users(username,email,password) VALUES(?,?,?)",
+    [username, email, password],
+    (err, result) => {
+      if (err) throw err;
+      res.send("Signup successful");
+    },
+  );
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE email=? AND password=?",
+    [email, password],
+    (err, result) => {
+      if (result.length > 0) {
+        res.send("Login success");
+      } else {
+        res.send("Invalid credentials");
+      }
+    },
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
